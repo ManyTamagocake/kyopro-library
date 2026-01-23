56b77805a7d267307100edd8b8fc2e3c4564b835
ManyTamagocake <manytamagocake@gmail.com>
2026-01-23 15:59:49 +0900

    Fixed eratosthenes.hpp

--- src/math/eratosthenes.hpp
+++ src/math/eratosthenes.hpp
@@ -2,8 +2,9 @@ vector<bool> eratosthenes(ll n){
     vector<bool> is_prime(n+1,true);
     is_prime[0] = false;
     is_prime[1] = false;
-    for (ll d = 2; d <= n; d++) {
-         for (ll i = d * 2; i <= n; i += d) {
+    for (ll d = 2; d * d <= n; d++) {
+        if (!is_prime[d]) continue;
+         for (ll i = d * d; i <= n; i += d) {
             is_prime[i] = false;
         }
     }
